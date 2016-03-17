848242f6f45830fb4cbd4a102500f0767dd7baab
Ran Shaham <shahamran91@gmail.com>
2016-03-17 10:09:13 +0200

    ex4 fill finished

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,58 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(CHECK)
+	@i
+	M=0
+	@KBD
+	D=M
+
+	@BLACK
+	D; JNE
+	
+	@i
+	M=0
+	D=M
+(WHITE)
+	// if i > 8,192:
+	//    goto CHECK
+	// (SCREEN_MEMORY_SIZE == 8192)
+	@8192
+	D=A
+	@i
+	D=M-D
+	@CHECK
+	D; JGE
+	
+	@i
+	D=M
+	@SCREEN
+	A=A+D
+	M=0
+	@i
+	M=M+1
+	@WHITE
+	0; JMP
+	
+(BLACK)
+	// if i > 8,192:
+	//    goto CHECK
+	// (SCREEN_MEMORY_SIZE == 8192)
+	@8192
+	D=A
+	@i
+	D=M-D
+	@CHECK
+	D; JGE
+	
+	@i
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+	@i
+	M=M+1
+	@BLACK
+	0; JMP
+
+		
